432ada4fc2c7e385fb091a7c87ab2ea335a9001c
gendaineko2222 <gendaineko2222@gmail.com>
2023-06-04 21:45:27 +0900

    Chore: textlint を導入

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -5,5 +5,6 @@ const buildEslintCommand = (filenames) =>
 
 module.exports = {
   '*.{js,jsx,ts,tsx}': [buildEslintCommand],
+  '*.{jsx,txt,md,mdx}': 'textlint --cache',
   '*': 'prettier --ignore-unknown --write',
 }
