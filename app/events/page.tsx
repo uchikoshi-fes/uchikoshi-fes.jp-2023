840d4134b735cd7f857fa4443d80c3bbf395b13e
ryo-ha <s2019195@asano.ed.jp>
2023-09-14 22:23:26 +0900

    Feat: イベント、アサノラジオのページを追加

---
+++ app/events/page.tsx
@@ -0,0 +1,28 @@
+import Image from 'next/image'
+import Link from '@components/common/link'
+import styles from './page.module.scss'
+import eventTable from 'public/event-table.jpg'
+
+export default function Page() {
+  return (
+    <article>
+      <div className={styles.article}>
+        <h1>イベント</h1>
+        <p>打越アリーナ、講堂、グラウンドで開催されるイベントのスケジュール表です。</p>
+        <p>
+          団体の詳細については<Link href='/orgs?category=公演'>こちら</Link>をご覧ください
+        </p>
+        <h2>スケジュール</h2>
+        <div className={styles.container}>
+          <section>
+            <Image
+              src={eventTable}
+              alt='イベントのスケジュール表'
+              style={{ width: '100%', height: 'auto' }}
+            ></Image>
+          </section>
+        </div>
+      </div>
+    </article>
+  )
+}
