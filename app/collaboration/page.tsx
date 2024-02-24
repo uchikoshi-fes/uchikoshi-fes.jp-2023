aa47bf68adc0261aa5ee38ce80df15d129387a49
ryo-ha <s2019195@asano.ed.jp>
2024-02-25 00:54:42 +0900

    Remove: komato-line-jpg

--- app/collaboration/page.tsx
+++ app/collaboration/page.tsx
@@ -32,7 +32,7 @@ export default function Page() {
         <li>
           <h2>駒場東邦文化祭 公式LINE</h2>
           <Link href='https://lin.ee/2X0uXb1'>
-            <Image src={komatoLine} alt='駒場東邦文化祭公式ライン QRコード'></Image>
+            {/* <Image src={komatoLine} alt='駒場東邦文化祭公式ライン QRコード'></Image> */}
             <p>https://lin.ee/2X0uXb1</p>
           </Link>
         </li>
